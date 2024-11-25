fc6a6284d1cdba1f1f59d3f8984e0b88e08502cb
Philip Yaw Boahene <boahenep22@gmail.com>
2024-11-25 12:59:12 -0800

    jquery exercise completed

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
-$(function() {
+$(function () {
   // YOUR CODE HERE
-})
+  $("#toggleButton").on("click", function () {
+    $("p").toggle("slow");
+  });
+  $("#colorButton").on("click", function () {
+    $("#colorDiv").css({
+      backgroundColor: "yellow",
+    });
+  });
+
+  $("#addClassButton").on("click", function () {
+    $("#classDiv").addClass("newClass");
+  });
+
+  $("#removeClassButton ").on("click", function () {
+    $("#classDiv").removeClass("newClass");
+  });
+
+  $("#fadeInButton").on("click", function () {
+    $("#fadeDiv").fadeIn();
+  });
+
+  $("#fadeOutButton").on("click", function () {
+    $("#fadeDiv").fadeOut();
+  });
+
+  $("#slideUpButton").on("click", function () {
+    $("#slideDiv").slideUp();
+  });
+
+  $("#slideDownButton").on("click", function () {
+    $("#slideDiv").slideDown();
+  });
+});
